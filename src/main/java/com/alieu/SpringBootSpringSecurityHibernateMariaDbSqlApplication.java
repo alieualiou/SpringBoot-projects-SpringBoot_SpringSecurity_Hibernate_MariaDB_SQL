159665611284e5231cbf0280ac08d2f79cfa45ad
alieualiou <88aliou@gmail.com>
2022-11-16 16:40:46 -0500

    first push after creating the Spring Boot project from the local computer

---
+++ src/main/java/com/alieu/SpringBootSpringSecurityHibernateMariaDbSqlApplication.java
@@ -0,0 +1,13 @@
+package com.alieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringSecurityHibernateMariaDbSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringSecurityHibernateMariaDbSqlApplication.class, args);
+	}
+
+}
